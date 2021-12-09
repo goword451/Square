f5069ba49ca0e081ffbaaa72344be93d26c43d59
Павлов Антон <goword45@gmail.com>
2021-12-09 20:09:26 +0300

    Переработка под .NET Framework 3.5

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ namespace Square
         [STAThread]
         static void Main()
         {
-            ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
     }
